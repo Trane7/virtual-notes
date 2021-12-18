17739dccfed55b2cd6c6b8102521d1b2eaa4fb88
Dee <dgrowette@gmail.com>
2021-12-18 11:17:33 -0800

    added a create a new note function to server.js file

--- server.js
+++ server.js
@@ -28,3 +28,38 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+
+
+// Create a new note function
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+    
+    if (notesArray.length === 0)
+        notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+}
+
+
+app.post('/api/notes', (req, res) => {
+    const newNote = createNewNote(req.body, allNotes);
+    res.json(newNote);
+});
+
+
+
+
+// Listening for the Port
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
