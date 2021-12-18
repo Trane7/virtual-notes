b733590748a23c0b689b5c73222cb88110f382f4
Dee <dgrowette@gmail.com>
2021-12-18 11:20:26 -0800

    added a delete note function

--- server.js
+++ server.js
@@ -56,7 +56,26 @@ app.post('/api/notes', (req, res) => {
     res.json(newNote);
 });
 
+// Delete notes function
+function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
 
+        if (note.id == id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify(notesArray, null, 2)
+            );
+            break;
+        }
+    }
+}
+
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, allNotes);
+    res.json(true);
+});
 
 
 // Listening for the Port
